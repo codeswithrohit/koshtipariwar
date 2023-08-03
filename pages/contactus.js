dbc25c1578a631375816eb2cec09f4e8747cc59a
codeswithrohit <110283451+codeswithrohit@users.noreply.github.com>
2023-08-03 13:36:07 +0530

    thutwo

--- pages/contactus.js
+++ pages/contactus.js
@@ -57,7 +57,7 @@ const Contactus = () => {
                     <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
                     <input
                       type="text"
-                      placeholder="John Doe"
+                      placeholder="Full Name"
                       value={fullName}
                       onChange={(e) => setFullName(e.target.value)}
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
@@ -68,7 +68,7 @@ const Contactus = () => {
                     <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
                     <input
                       type="email"
-                      placeholder="johndoe@example.com"
+                      placeholder="example@example.com"
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
